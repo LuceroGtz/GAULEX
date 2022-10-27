db43065104c45b7432afcde1145ecff8fe63d9c0
Lucero Gtz <chero_ito@hotmail.com>
2022-10-27 00:47:48 -0500

    agregar proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
+import { v4 as uuidv4 } from "uuid";
 import {TodoList} from './TodoList';
 
 
+
     export function App() {
-        const [todos, setTodos]= useState();
+        const [todos, setTodos]= useState([
+            {id: 1, task: "Proyecto 1", completed: false},
+        ]);
+        const todoTaskRef = useRef ();
+
+        const handleTodoAdd = ()=> {
+        const task = todoTaskRef.current.value;
+        if (task == "") return;
+
+        setTodos((prevTodos) => {
+            return [... prevTodos, [id = uuidv4, task, completed =false]]
+        });
+
+        todoTaskRef.current.value = null;
+
+        }
         
         return (
         <React.Fragment>
             <TodoList todos= {todos} /> 
-            <input type="text" placeholder="Nuvo Proyectyo" />
-            <button>+</button>
+            <input ref= {todoTaskRef} type="text" placeholder="Nuvo Proyectyo" />
+            <button onClick={handleTodoAdd }>+</button>
         </React.Fragment>
         )
         }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { TodoItems } from ".TodoItems";
 
-export default function TodoList( {todos}) {
+export function TodoList ( {todos}) {
   return (
     <ul>
         {todos.map ((todo) => (
